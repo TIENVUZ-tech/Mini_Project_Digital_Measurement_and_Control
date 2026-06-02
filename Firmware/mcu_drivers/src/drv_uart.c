0cc1bf33fa6c4a8b69bcfc2c99d880eac0f1af2b
TIENVUZ-tech <tien3152005@gmail.com>
2026-06-02 11:26:52 +0700

    Swap drv_uart.h and drv_uart.c

--- Firmware/mcu_drivers/src/drv_uart.c
+++ Firmware/mcu_drivers/src/drv_uart.c
@@ -1,13 +1,57 @@
-#ifndef DRV_UART_H_
-#define DRV_UART_H_
+#include "drv_uart.h"
 
-#include "stm32f10x.h"
+void DRV_UART1_Init(uint32_t baudrate) {
+	// Enable UART1 and GPIOA
+	RCC->APB2ENR |= (1 << 14) | (1 << 2);
 
+	// Config PA9 (TX) and PA10 (RX)
+	GPIOA->CRH &= ~(0xFF << 4);
+	GPIOA->CRH |= 0xB << 4; // PA9, max speed = 50, AFO push-pull
+	GPIOA->CRH |= 0x4 << 8; // PA10, max speed = 50, Floating input
 
-void DRV_UART1_Init(uint32_t baudrate);
-void DRV_UART1_SendByte(uint8_t data);
-void DRV_UART1_SendBytes(uint8_t *data, uint16_t len);
-void DRV_UART1_SendString(char *str);
-uint8_t DRV_UART1_Available(void);
-uint8_t DRV_UART1_ReadByte(void);
-#endif
+	uint32_t fclk = 72e6;
+
+	// Config baudrate
+	float usart_div = (float)fclk / (16.0f * baudrate);
+	uint16_t mantisa = (uint16_t)usart_div;
+	uint8_t fraction = (uint8_t)(((usart_div - mantisa) * 16.0f) + 0.5f);
+
+	if (fraction >= 16) {
+		mantisa += 1;
+		fraction = 0;
+	}
+
+	USART1->BRR = (mantisa << 4) | (fraction);
+
+	// Enable USART
+	USART1->CR1 |= (1 << 13) | (1 << 3) | (1 << 2);
+}
+
+void DRV_UART1_SendByte(uint8_t data) {
+	while (!(USART1->SR & (1 << 7)));
+	USART1->DR = data;
+}
+
+void DRV_UART1_SendBytes(uint8_t *data, uint16_t len) {
+	if (len == 0) return;
+	for (uint16_t i = 0; i < len; i++) {
+		DRV_UART1_SendByte(data[i]);
+	}
+}
+
+void DRV_UART1_SendString(char *str) {
+	while (*str) {
+		DRV_UART1_SendByte(*str++);
+	}
+}
+
+uint8_t DRV_UART1_Available(void) {
+	if (USART1->SR & (1 << 5)) {
+		return 1;
+	}
+	return 0;
+}
+uint8_t DRV_UART1_ReadByte(void) {
+	return USART1->DR;
+}
+
