b9513e16bce09fc2ca0a460bfc83bc9a336d1258
FunnGame <tatthanh230405@gmail.com>
2026-06-03 22:40:24 +0700

    sửa

--- Firmware/mcu_drivers/inc/drv_pwm.h
+++ Firmware/mcu_drivers/inc/drv_pwm.h
@@ -1,19 +1,32 @@
-#ifndef DRV_PWM_H_
-#define DRV_PWM_H_
+#ifndef __DRV_PWM_H
+#define __DRV_PWM_H
 
-#include <stdint.h> 
+#include "stm32f1xx.h"
+#include <stdint.h>
+#include "drv_gpio.h" 
+
+typedef enum {
+    PWM_TIM1, 
+    PWM_TIM2, 
+    PWM_TIM3, 
+    PWM_TIM4  
+} PWM_Timer;
+
+typedef enum {
+    PWM_CH1 = 1,
+    PWM_CH2 = 2,
+    PWM_CH3 = 3,
+    PWM_CH4 = 4
+} PWM_Channel;
 
 
+void DRV_PWM_Init(PWM_Timer tim, Ports port, uint8_t pin, PWM_Channel channel, uint32_t frequency);
 
-void DRV_PWM_Init(void);
-
-
-void DRV_PWM_Start(uint8_t channel);
+void DRV_PWM_Start(PWM_Timer tim, PWM_Channel channel);
 
 
-void DRV_PWM_SetDuty(uint8_t channel, uint16_t duty);
+void DRV_PWM_SetDuty(PWM_Timer tim, PWM_Channel channel, uint16_t duty);
 
+void DRV_PWM_Stop(PWM_Timer tim, PWM_Channel channel);
 
-void DRV_PWM_Stop(uint8_t channel);
-
-#endif 
+#endif
