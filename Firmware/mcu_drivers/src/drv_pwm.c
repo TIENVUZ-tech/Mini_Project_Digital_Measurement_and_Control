eed34055b6696bcef1636ad18cc637be7c2b460e
FunnGame <tatthanh230405@gmail.com>
2026-06-03 22:55:33 +0700

    sửa

--- Firmware/mcu_drivers/src/drv_pwm.c
+++ Firmware/mcu_drivers/src/drv_pwm.c
@@ -1,66 +1,102 @@
 #include "drv_pwm.h"
-#include "stm32f103xb.h"
 
-/* ========================================================================= */
-/* HÀM 1: Khởi tạo bộ Timer 3 chạy chế độ PWM 1 tại chân PA6                */
-/* ========================================================================= */
-void DRV_PWM_Init(void) {
-    // 1. Bật Clock hệ thống
-    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
-    RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
-
-    // 2. Cấu hình chân PA6 làm Alternate Function Push-Pull 50MHz
-    GPIOA->CRL &= ~(0xF << 24);
-    GPIOA->CRL |=  (0xB << 24); 
-
-    // 3. Cấu hình tần số đếm (PSC=71, ARR=999 -> Tần số PWM = 1kHz)
-    TIM3->PSC = 71;           
-    TIM3->ARR = 999;          
-
-    // 4. Cấu hình chế độ PWM Mode 1 và bật Preload cho Channel 1
-    TIM3->CCMR1 &= ~(TIM_CCMR1_OC1M | TIM_CCMR1_OC1PE);
-    TIM3->CCMR1 |= (0x6 << TIM_CCMR1_OC1M_Pos) | TIM_CCMR1_OC1PE;
-
-    // 5. Mặc định ban đầu tắt xung (Duty = 0%)
-    TIM3->CCR1 = 0;
-
-    // BAN ĐẦU CHƯA CHO PHÁT XUNG (Để hàm Start lo)
-    TIM3->CCER &= ~TIM_CCER_CC1E;
-    TIM3->CR1  &= ~TIM_CR1_CEN;
-}
-
-/* ========================================================================= */
-/* HÀM 2: Bắt đầu phát xung PWM                                             */
-/* ========================================================================= */
-void DRV_PWM_Start(uint8_t channel) {
-    if (channel == 1) {
-        TIM3->CCER |= TIM_CCER_CC1E; // Cho phép xuất xung ra chân PA6
-        TIM3->CR1  |= TIM_CR1_CEN;  // Cho phép bộ đếm Timer chạy
+static TIM_TypeDef* Get_Timer_Instance(PWM_Timer tim) {
+    switch (tim) {
+        case PWM_TIM1: return TIM1;
+        case PWM_TIM2: return TIM2;
+        case PWM_TIM3: return TIM3;
+        case PWM_TIM4: return TIM4;
+        default: return TIM2;
     }
 }
 
-/* ========================================================================= */
-/* HÀM 3: Set Duty từ 0 đến 100% (Quy đổi thông minh)                       */
-/* ========================================================================= */
-void DRV_PWM_SetDuty(uint8_t channel, uint16_t duty) {
-    if (channel == 1) {
-        // Khống chế giá trị đầu vào tối đa là 100%
-        if (duty > 100) duty = 100;
-        
-        // Ép kiểu sang uint32_t khi tính toán để tuyệt đối an toàn, tránh tràn số
-        uint32_t ccr_value = ((uint32_t)duty * 999) / 100;
-        
-        TIM3->CCR1 = ccr_value; 
+void DRV_PWM_Init(PWM_Timer tim, Ports port, uint8_t pin, PWM_Channel channel, uint32_t frequency) {
+    RCC->APB2ENR |= (1 << (port + 2));
+
+    if (tim == PWM_TIM1) {
+        RCC->APB2ENR |= (1 << 11);
+    } else {
+        RCC->APB1ENR |= (1 << (tim - 1));
+    }
+
+    GPIO_TypeDef *gpio_port;
+    if (port == PA)       gpio_port = GPIOA;
+    else if (port == PB)  gpio_port = GPIOB;
+    else                  gpio_port = GPIOC;
+
+    uint8_t bit_pos = (pin % 8) * 4;
+    if (pin < 8) {
+        gpio_port->CRL &= ~(0x0FUL << bit_pos);
+        gpio_port->CRL |= (0x0AUL << bit_pos);
+    } else {
+        gpio_port->CRH &= ~(0x0FUL << bit_pos);
+        gpio_port->CRH |= (0x0AUL << bit_pos);
+    }
+
+    TIM_TypeDef *TIMx = Get_Timer_Instance(tim);
+    TIMx->PSC = 71;
+    TIMx->ARR = (1000000UL / frequency) - 1;
+
+    switch (channel) {
+        case PWM_CH1:
+            TIMx->CCMR1 &= ~(0x7UL << 4);
+            TIMx->CCMR1 |= (0x6UL << 4) | (1UL << 3);
+            break;
+        case PWM_CH2:
+            TIMx->CCMR1 &= ~(0x7UL << 12);
+            TIMx->CCMR1 |= (0x6UL << 12) | (1UL << 11);
+            break;
+        case PWM_CH3:
+            TIMx->CCMR2 &= ~(0x7UL << 4);
+            TIMx->CCMR2 |= (0x6UL << 4) | (1UL << 3);
+            break;
+        case PWM_CH4:
+            TIMx->CCMR2 &= ~(0x7UL << 12);
+            TIMx->CCMR2 |= (0x6UL << 12) | (1UL << 11);
+            break;
+    }
+
+    TIMx->CR1 |= (1 << 7);
+    TIMx->CR1 |= (1 << 0);
+}
+
+void DRV_PWM_Start(PWM_Timer tim, PWM_Channel channel) {
+    TIM_TypeDef *TIMx = Get_Timer_Instance(tim);
+
+    switch (channel) {
+        case PWM_CH1: TIMx->CCER |= (1 << 0);  break;
+        case PWM_CH2: TIMx->CCER |= (1 << 4);  break;
+        case PWM_CH3: TIMx->CCER |= (1 << 8);  break;
+        case PWM_CH4: TIMx->CCER |= (1 << 12); break;
+    }
+
+    if (tim == PWM_TIM1) {
+        TIMx->BDTR |= (1 << 15);
     }
 }
 
-/* ========================================================================= */
-/* HÀM 4: Dừng phát xung PWM                                                */
-/* ========================================================================= */
-void DRV_PWM_Stop(uint8_t channel) {
-    if (channel == 1) {
-        TIM3->CCR1 = 0;              // Đưa công suất về 0%
-        TIM3->CCER &= ~TIM_CCER_CC1E; // Ngắt xuất xung ra chân vật lý
-        TIM3->CR1  &= ~TIM_CR1_CEN;  // Dừng bộ đếm Timer để tiết kiệm điện
+void DRV_PWM_SetDuty(PWM_Timer tim, PWM_Channel channel, uint16_t duty) {
+    if (duty > 100) duty = 100;
+
+    TIM_TypeDef *TIMx = Get_Timer_Instance(tim);
+
+    uint32_t ccr_value = ((TIMx->ARR + 1) * duty) / 100;
+
+    switch (channel) {
+        case PWM_CH1: TIMx->CCR1 = ccr_value; break;
+        case PWM_CH2: TIMx->CCR2 = ccr_value; break;
+        case PWM_CH3: TIMx->CCR3 = ccr_value; break;
+        case PWM_CH4: TIMx->CCR4 = ccr_value; break;
+    }
+}
+
+void DRV_PWM_Stop(PWM_Timer tim, PWM_Channel channel) {
+    TIM_TypeDef *TIMx = Get_Timer_Instance(tim);
+
+    switch (channel) {
+        case PWM_CH1: TIMx->CCER &= ~(1 << 0);  break;
+        case PWM_CH2: TIMx->CCER &= ~(1 << 4);  break;
+        case PWM_CH3: TIMx->CCER &= ~(1 << 8);  break;
+        case PWM_CH4: TIMx->CCER &= ~(1 << 12); break;
     }
 }
