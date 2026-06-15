fb13135359207aa503e1e03f356c665a333b8bb1
FunnGame <tatthanh230405@gmail.com>
2026-06-15 18:38:24 +0700

    Sửa

--- Firmware/platform/inc/actuator_hal.h
+++ Firmware/platform/inc/actuator_hal.h
@@ -1,20 +1,20 @@
-##ifndef __ACTUATOR_HAL_H
+#ifndef __ACTUATOR_HAL_H
 #define __ACTUATOR_HAL_H
 
 #include <stdint.h>
-
 
 typedef enum {
     UNIT_PERCENT,
     UNIT_RPM,
     UNIT_RPS,
-    UNIT_LUX,
-    UNIT_RAW
+    UNIT_Do_C,    
+    UNIT_Do_F,
+    UNIT_Do_K,     
 } ActuatorType_t;
 
 void ActuatorHAL_Init(void);
 
 void ActuatorHAL_SetFan(ActuatorType_t type, float value);
-void ActuatorHAL_SetLight(ActuatorType_t type, float value);
+void ActuatorHAL_SetHeater(ActuatorType_t type, float value);
 
 #endif
