32169a017e7ce65672ff992eacb16c1a041b8f7f
23020867-boop <23020867@vnu.edu.vn>
2026-06-16 07:07:51 +0700

    Refactor timer delay functions and initialization

--- Firmware/mcu_drivers/src/drv_timer.c
+++ Firmware/mcu_drivers/src/drv_timer.c
@@ -1,58 +1,49 @@
 #include "stm32f10x.h"
 #include "../inc/drv_timer.h"
 
-void Delay_us(Timers timer, int us){
-	int prescaler = 72 - 1;
-	TIM_TypeDef *tim;
+#include "stm32f10x.h"
+#include "drv_timer.h"
 
-  //Choose timer
-	if(timer == 1) RCC->APB2ENR |= 1 << 11;
-	else RCC->APB1ENR |= 1 << (timer - 2);
-	
-	switch(timer){
-		case 1: tim = TIM1;
-			break;
-		case 2: tim = TIM2;
-			break;
-		case 3: tim = TIM3;
-			break;
-		case 4: tim = TIM4;
-			break;
-		default: return;
-	}
+volatile uint32_t tick = 0;
 
-  //Set up for timer
-	tim->PSC = prescaler;
-	tim->ARR = us - 1;
-	tim->CNT = 0;
-	tim->SR &= ~(1 << 0);
-	tim->CR1 |= 1 << 0;
-	while(!(tim->SR & 1 << 0));
-	tim->CR1 &= ~(1 << 0);
-	tim->SR &= ~(1 << 0);
+void timerInitForDelay(void){
+	//Su dung TIM4 de tao ham delay
+	RCC->APB1ENR |= 1 << 2;
+	TIM4->PSC = 72 - 1;
+	TIM4->EGR |= 1 << 0;
+	TIM4->CR1 |= 1 << 0;
 }
 
-void Delay_ms(Timers timer, int ms){
-	for(int i = 0; i < ms; i++)
-		Delay_us(timer, 1000);
+void SystickInit(void){
+	SysTick->LOAD = 72000 - 1;
+	SysTick->VAL = 0;
+	SysTick->CTRL |=  (1 << 2) | (1 << 1) | (1 << 0);
 }
+
 
 void delay_us(int us){
-	Delay_us(T4, us);
+	TIM4->ARR = us - 1;
+	TIM4->CNT = 0;
+	TIM4->SR &= ~(1 << 0);
+	while(!(TIM4->SR & 1 << 0));
+	TIM4->SR |= 1 << 0;
 }
 
 void delay_ms(int ms){
-	Delay_ms(T4, ms);
+	for(int i = 0; i < ms; i++){
+		delay_us(1000);
+	}
 }
+
 
 /*
 void TIM2_Init(void) {
-    RCC->APB1ENR |= (1 << 0);   // ? thêm dòng này
+    RCC->APB1ENR |= (1 << 0);   // ? thÃªm dÃ²ng nÃ y
 
     TIM2->PSC = 72 - 1;         // 72MHz / 72 = 1MHz
     TIM2->ARR = 1000 - 1;       // 1MHz / 1000 = 1ms per tick
     TIM2->CNT = 0;
-    TIM2->SR  &= ~(1 << 0);     // xóa UIF tru?c khi b?t
+    TIM2->SR  &= ~(1 << 0);     // xÃ³a UIF tru?c khi b?t
     TIM2->DIER |= (1 << 0);     // UIE
     TIM2->CR1  |= (1 << 0);     // CEN - start timer
     NVIC_EnableIRQ(TIM2_IRQn);
