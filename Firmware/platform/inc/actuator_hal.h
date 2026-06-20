fc684a87d0dd97c67ad3a6f185b21e165fa6936f
FunnGame <tatthanh230405@gmail.com>
2026-06-20 16:35:09 +0700

    Sửa

--- Firmware/platform/inc/actuator_hal.h
+++ Firmware/platform/inc/actuator_hal.h
@@ -4,7 +4,9 @@
 
 typedef enum {
     ACTUATOR_FAN    = 0,
-    ACTUATOR_HEATER = 1
+    ACTUATOR_HEATER = 1,
+	  ACTUATOR_MICRO  = 2,
+	  ACTUATOR_LIGHT  = 3
 } ActuatorDevice_t;
 
 typedef enum {
@@ -18,7 +20,8 @@ typedef enum {
     UNIT_RPS,
     UNIT_DEG_C,
     UNIT_DEG_F,
-    UNIT_DEG_K
+    UNIT_DEG_K,
+	  UNIT_LUX
 } ActuatorUnit_t;
 
 
@@ -29,7 +32,16 @@ typedef struct {
     float            value;  
 } ActuatorPrams_struct_t;
 
+
+typedef enum {
+    ACTUATOR_OK = 0,
+    ACTUATOR_ERROR_INDEX,
+    ACTUATOR_ERROR_MODE,
+    ACTUATOR_ERROR_UNIT,
+    ACTUATOR_ERROR_VALUE
+} errorCode;
+
 void ActuatorHAL_Init(void);
-void ActuatorHAL_Set(ActuatorPrams_struct_t actuatorPrams);
+errorCode ActuatorHAL_Set(ActuatorPrams_struct_t actuatorPrams);
 
 #endif
