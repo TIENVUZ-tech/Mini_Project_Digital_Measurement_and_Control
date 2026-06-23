f8fbd7205b6a009c952656af17e14f7a152f09bf
TIENVUZ-tech <tien3152005@gmail.com>
2026-06-23 14:21:31 +0700

    Update drv_uart.c

--- Firmware/mcu_drivers/src/drv_uart.c
+++ Firmware/mcu_drivers/src/drv_uart.c
@@ -1,16 +1,17 @@
 #include "../inc/drv_uart.h"
 #include "../../platform/inc/comm_hal.h"
 
+#define USART_BRR_DIV_Mantissa_Pos 4
 void DRV_UART1_Init(uint32_t baudrate) {
 	// Enable UART1 and GPIOA
-	RCC->APB2ENR |= (1 << 14) | (1 << 2);
+	RCC->APB2ENR |= RCC_APB2ENR_USART1EN | RCC_APB2ENR_IOPAEN;
 
 	// Config PA9 (TX) and PA10 (RX)
-	GPIOA->CRH &= ~(0xFF << 4);
-	GPIOA->CRH |= 0xB << 4; // PA9, max speed = 50, AFO push-pull
-	GPIOA->CRH |= 0x4 << 8; // PA10, max speed = 50, Floating input
+	GPIOA->CRH &= ~(GPIO_CRH_MODE9 | GPIO_CRH_CNF9 | GPIO_CRH_MODE10 | GPIO_CRH_CNF10);
+	GPIOA->CRH |= (GPIO_CRH_MODE9_0 | GPIO_CRH_MODE9_1 | GPIO_CRH_CNF9_1); // PA9, max speed = 50, AFO push-pull
+	GPIOA->CRH |= GPIO_CRH_CNF10_0; // PA10, max speed = 50, Floating input
 
-	uint32_t fclk = 72e6;
+	uint32_t fclk = 72000000UL;
 
 	// Config baudrate
 	float usart_div = (float)fclk / (16.0f * (float)baudrate);
@@ -22,10 +23,10 @@ void DRV_UART1_Init(uint32_t baudrate) {
 		fraction = 0;
 	}
 
-	USART1->BRR = (mantisa << 4) | (fraction);
+	USART1->BRR = (mantisa << USART_BRR_DIV_Mantissa_Pos) | (fraction);
 
 	// Enable UE, TE, RE, RXNEIE
-	USART1->CR1 |= (1 << 13) | (1 << 3) | (1 << 2) | (1 << 5);
+	USART1->CR1 |= USART_CR1_UE | USART_CR1_TE | USART_CR1_RE | USART_CR1_RXNEIE;
 	
 	// Enable USART1 interrupt at NVIC
 	NVIC_EnableIRQ(USART1_IRQn);
@@ -33,14 +34,14 @@ void DRV_UART1_Init(uint32_t baudrate) {
 
 // IRQ Handler
 void USART1_IRQHandler(void) {
-	if (USART1->SR & (1 << 5)) {
+	if (USART1->SR & USART_SR_RXNE) {
 		uint8_t byte = (uint8_t)USART1->DR;
 		CommHAL_RxByteCallback(byte);
 	}
 }
 		
 void DRV_UART1_SendByte(uint8_t data) {
-	while (!(USART1->SR & (1 << 7)));
+	while (!(USART1->SR & USART_SR_TXE));
 	USART1->DR = data;
 }
 
@@ -50,4 +51,3 @@ void DRV_UART1_SendBytes(uint8_t *data, uint8_t len) {
 		DRV_UART1_SendByte(data[i]);
 	}
 }
-
