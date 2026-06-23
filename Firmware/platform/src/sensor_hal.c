baf1f7221636803710e6406caf5b6252f1090e41
23020867-boop <23020867@vnu.edu.vn>
2026-06-23 10:18:58 +0700

    Implement DHT22 data reading functions
    Added functions to read humidity and temperature data from the DHT22 sensor.

--- Firmware/platform/src/sensor_hal.c
+++ Firmware/platform/src/sensor_hal.c
@@ -1,7 +1,11 @@
 #include "../inc/sensor_hal.h"
 #include "../../mcu_drivers/inc/drv_timer.h"
 
+float Humidity;
+float Temperature;
+
 void DHT22_Start(Ports port, unsigned short pin){
+	timerInitForDelay();
 	GPIO_Init(port, pin, OUT50, O_GP_PP);
 	GPIO_Write(port, pin, 0);
 	delay_ms(2);
@@ -54,4 +58,30 @@ uint8_t DHT22_Read_Byte(Ports port, unsigned short pin){
 	return data;
   
 }
+
+
+void DHT22_ReadData(){
+	DHT22_Start(DHT22_Port, DHT22_Pin);
+	if(DHT22_Check_Response(DHT22_Port, DHT22_Pin) == 1){
+		DHT22_Data[0] = DHT22_Read_Byte(DHT22_Port, DHT22_Pin); //Do am nguyen
+		DHT22_Data[1] = DHT22_Read_Byte(DHT22_Port, DHT22_Pin); //Do am thap phan
+		DHT22_Data[2] = DHT22_Read_Byte(DHT22_Port, DHT22_Pin); //Nhiet do nguyen
+		DHT22_Data[3] = DHT22_Read_Byte(DHT22_Port, DHT22_Pin); //Nhiet do thap phan
+		DHT22_Data[4] = DHT22_Read_Byte(DHT22_Port, DHT22_Pin); //Check sum
+	}
+
+	uint8_t sum = DHT22_Data[0] + DHT22_Data[1] +DHT22_Data[2] + DHT22_Data[3];
+	if(DHT22_Data[4] == sum){
+		uint16_t rawHumidity = (DHT22_Data[0] << 8) | DHT22_Data[1];
+		uint16_t rawTemperature = (DHT22_Data[2] << 8) | DHT22_Data[3];
+		Humidity = (float) rawHumidity/10.0;
+		Temperature = (float) rawTemperature/10.0;
+	}
+
+}
+
+float readTemperature(){
+	return Temperature;
+}
+
 
